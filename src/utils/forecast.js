582ad01dd3c7723fd8f44fac822f6a064396a685
muskan <muskankalra321@gmail.com>
2019-06-15 12:02:31 +0530

    modification in forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ request({url , json: true}, (error, {body})=>{
     }else if(body.error){
         callback('Unable to find location', undefined)
     }else{
-        callback( undefined, body.daily.data[0].summary + ' It is currently ' +body.currently.temperature +' degrees out. There is a '+  body.currently.precipProbability  +  '% chance of a rain')
+        callback( undefined, body.daily.data[0].summary + '  with a temerature high of '+ body.daily.data[0].temperatureHigh+'  degree and a temperature low of '+  body.daily.data[0].temperatureLow+' degree.'+  ' It is currently ' +body.currently.temperature +' degrees out. There is a '+  body.currently.precipProbability  +  '% chance of a rain')
      }
     })
 }
